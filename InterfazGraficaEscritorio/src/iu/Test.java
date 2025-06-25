fc513a4a92efa3e951b7eeea6e63c44119ba5fe5
Julio Peraza <julioperaza95@gmail.com>
2025-06-25 16:34:44 -0300

    tercer proyecto

---
+++ InterfazGraficaEscritorio/src/iu/Test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iu;
+
+/**
+ *
+ * @author peraza
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.setVisible(true);
+    }
+    
+}
